85cb66c8a7be23ed7575cbc0f4297ed5cfad9214
Nils Werner <nils.werner@gmail.com>
2009-05-26 15:10:50 +0200

    Initial Version

---
+++ README.txt
@@ -0,0 +1,22 @@
+
+Enable Tabkey
+------------------------------------
+
+Version: 1.0
+Author: Nils Werner (nils.werner@gmail.com)
+Build Date: 2009-05-26
+Requirements: Symphony 2.02
+
+
+[INSTALLATION]
+
+1. Upload the 'enable_tabkey' folder in this archive to your Symphony 'extensions' folder.
+
+2. Enable it by selecting the "Enable Tabkey" entry, choose Enable from the with-selected menu, then click Apply.
+
+3. You can now use the Tabkey in your code editors.
+
+[NOTES]
+
+- Currently incompatible with the "markItUp!"-extension, hopefully that will be resolved in Symphony 2.0.3 and its unified JS-libraries.
+  If its my fault and I'm also breaking other extensions, please let me know!
---
+++ extension.driver.php
@@ -0,0 +1,36 @@
+<?php
+
+	Class extension_enable_tabkey extends Extension{
+	
+		public function about(){
+			return array('name' => 'Enable Tabkey',
+						 'version' => '1.0',
+						 'release-date' => '2009-05-26',
+						 'author' => array('name' => 'Nils Werner',
+										   'website' => 'http://www.phoque.com/projekte/symphony',
+										   'email' => 'nils.werner@gmail.com')
+				 		);
+		}
+		
+		
+		public function getSubscribedDelegates() {
+			return array(
+				array(
+					'page'		=> '/backend/',
+					'delegate'	=> 'InitaliseAdminPageHead',
+					'callback'	=> 'initaliseAdminPageHead'
+				)
+			);
+		}
+
+		public function initaliseAdminPageHead($context) {
+			$page = $context['parent']->Page;
+			
+			$page->addScriptToHead(URL . '/extensions/enable_tabkey/assets/jquery.js', 3066701);
+			$page->addScriptToHead(URL . '/extensions/enable_tabkey/assets/enable_tabkey.js', 3066702);
+			$page->addScriptToHead(URL . '/extensions/enable_tabkey/assets/tabby.js', 3066703);
+		}
+			
+	}
+
+?>
